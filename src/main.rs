aa569b7dddcfc6edd37125926b61687fd33f4e5b
Mayk <7395296+y0k4i-1337@users.noreply.github.com>
2023-06-28 13:34:51 -0300

    Add command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser};
+
+#[derive(Debug, Parser)]
+#[command(author, about, version, long_about = None)]
+struct Cli {
+    /// Nameservers to use (comma-separated list or file)
+    #[arg(short, long)]
+    nameservers: String,
+
+    /// SOCKS proxy to use (format: "address:port")
+    #[arg(short = 'x', long)]
+    proxy: String,
+
+    /// Server address to listen on
+    #[arg(short, long, default_value = "127.0.0.1")]
+    address: String,
+
+    /// Server port to listen on
+    #[arg(short, long, default_value_t = 53, value_parser = clap::value_parser!(u16).range(1..))]
+    port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("{:?}", cli);
 }
